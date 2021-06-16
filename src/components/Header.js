5732e819d3ab9399ef34ec002ac9f61c90d97b0e
TaariqM <taariqm9@my.yorku.ca>
2021-06-15 23:20:21 -0400

    Added navigation buttons. Still need to add more styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import "../css/Header.css";
+import Navigation from "./Navigation";
 
 const Header = () => {
   return (
     <div className="segment">
       <div className="segment__nba-header">NBA Base</div>
+      <Navigation />
       {/* <h1 className='ui header'>NBA Base</h1> */}
     </div>
   );
---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../css/Navigation.css";
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navbtn-container">
+        <button className="navbtn-container__link">TEAMS</button>
+        <button className="navbtn-container__link">PLAYERS</button>
+      </div>
+    </>
+  );
+};
+
+export default Navigation;
